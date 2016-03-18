b32fdd2211353a3c983b94feb59286ac35c953f1
wanghuafeng <wanghuafengc@163.com>
2016-03-18 16:43:14 +0800

    del

--- crawler-ds/src/main/java/com/rong360/crawler/ds/processor/store/AlipayOrderStore.java
+++
@@ -1,7 +0,0 @@
-package com.rong360.crawler.ds.processor.store;
-
-/**
- * Created by Administrator on 2015/10/30.
- */
-public class AlipayOrderStore {
-}
--- crawler-ds/src/main/java/com/rong360/crawler/ds/service/DSApiResultService.java
+++
@@ -1,7 +0,0 @@
-package com.rong360.crawler.ds.service;
-
-import com.rong360.crawler.ds.bean.DSApiResult;
-
-public interface DSApiResultService {
-    void save(DSApiResult dsApiResult);
-}
